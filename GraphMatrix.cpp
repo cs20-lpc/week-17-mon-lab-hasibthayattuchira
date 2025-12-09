a41620f5ac568bd8e45152da4965d27e4910ff44
hasibthayattuchira <hthayattuchira@gmail.com>
2025-12-08 20:58:59 -0800

    Finished Lab

--- GraphMatrix.cpp
+++ GraphMatrix.cpp
@@ -2,14 +2,30 @@
 
 GraphMatrix::GraphMatrix(int numV, GType type) {
     // TODO
+    v = numV;
+    adjMatrix = new bool*[v];
+    for(int i = 0; i < v; i++){
+        adjMatrix[i] = new bool[v];
+        for(int j = 0; j < v; j++){
+            adjMatrix[i][j] = false;
+        }
+    }
 }
 
 GraphMatrix::~GraphMatrix() {
     // TODO
+    for(int i = 0; i < v; i++){
+        delete[] adjMatrix[i];
+    }
+    delete[] adjMatrix;
 }
 
 bool GraphMatrix::isEdge(int i, int j) const {
     // TODO
+    if(i < 0 || i >= v || j < 0 || j >=v){
+        throw runtime_error("Error in isEdge: index out of bounds");
+    }
+    return adjMatrix[i][j];
 }
 
 ostream& GraphMatrix::print(ostream& os) const {
@@ -38,4 +54,19 @@ ostream& GraphMatrix::print(ostream& os) const {
 
 void GraphMatrix::toggleEdge(int i, int j) {
     // TODO
+    if(i < 0 || i >= v || j < 0 || j >= v){
+        throw runtime_error("Error in toggleEdge: index out of bounds");
+    }
+
+    bool oldValue = adjMatrix[i][j];
+    adjMatrix[i][j] = !adjMatrix[i][j];
+
+    if(adjMatrix[i][j]){
+        e++;
+    } else {
+        e--;
+    }
+    if(graphType == UNDIRECTED && i != j){
+        adjMatrix[j][i] = adjMatrix[i][j];
+    }
 }
